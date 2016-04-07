831773ef399ca4e6b3f0324cfc17b516840ad8a9
Mehdy Dara <mdara@eleven-labs.com>
2016-04-07 18:07:34 +0200

    Enable coverage for webpack build

--- generators/app/conf.js
+++ generators/app/conf.js
@@ -72,6 +72,11 @@ module.exports = function karmaConf(props) {
   }
 
   if (props.modules === 'webpack') {
+    conf.reporters = lit`['progress', 'coverage']`;
+    conf.coverageReporter = {
+      type: lit`html`,
+      dir: lit`'coverage/'`
+    };
     conf.webpack = lit`require('./webpack-test.conf.js')`;
     conf.webpackMiddleware = { noInfo: true };
   }
