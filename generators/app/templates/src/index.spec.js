8ed2d11f1968d0a788ead4d839ce5e12b41e3d2e
Micaël <micael.mbagira@icloud.com>
2016-09-01 09:20:08 +0200

    rc5

--- generators/app/templates/src/index.spec.js
+++ generators/app/templates/src/index.spec.js
@@ -14,5 +14,5 @@ context.keys().forEach(context);
 const testing = require('@angular/core/testing');
 const testingBrowser = require('@angular/platform-browser-dynamic/testing');
 
-testing.setBaseTestProviders(testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+testing.TestBed.initTestEnvironment(testingBrowser.BrowserDynamicTestingModule, testingBrowser.platformBrowserDynamicTesting());
 <% } -%>
